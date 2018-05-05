a3a4fc0ac9b61d964699aa72baaa35300f7f2190
IgorSpi <igor13pao@hotmail.com>
2018-05-05 22:43:00 +0300

    Minimum Esge of Node

--- app/src/main/java/com/example/igor/rugetsahackathon2018/Node.java
+++ app/src/main/java/com/example/igor/rugetsahackathon2018/Node.java
@@ -1,27 +1,58 @@
 package com.example.igor.rugetsahackathon2018;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class Node {
     private String name;
     private HashMap<String,Edge> neighborsHM = new HashMap<>();
+    private HashMap<Double,Edge> neighborsDistanceHM = new HashMap<>();
+    private ArrayList<Edge> edgesSorted = new ArrayList<>();
 
     private float latitude;
     private float longitude;
 
+    private boolean isVisited;
+
+    public boolean isVisited() {
+        return isVisited;
+    }
 
     public Node(String name, float latitude, float longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.isVisited=false;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setVisited(boolean visited) {
+        this.isVisited = visited;
+    }
+
+    public HashMap<Double, Edge> getNeighborsDistanceHM() {
+        return neighborsDistanceHM;
+    }
+
+
     public HashMap<String,Edge> getNeighbors() {
         return neighborsHM;
+    }
+    public void addNeighborsWithDistance()
+    {
+        Set set = neighborsHM.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mEntry = (Map.Entry) iterator.next();
+            Edge edge = (Edge) mEntry.getValue();
+            neighborsDistanceHM.put(edge.getWeight(),edge);
+        }
     }
     public Edge getNeighbor(String neighborName)
     {
@@ -43,6 +74,7 @@ public class Node {
     public void addNeighbor(Edge neighbor){
         neighborsHM.put(neighbor.getNeighbor().getName(),neighbor);
     }
+
     public void clearNeighbors(){
         neighborsHM.clear();
     }
@@ -51,7 +83,28 @@ public class Node {
         this.latitude = latitude;
         this.longitude = longitude;
     }
+    public void addEdgesSorted(Edge edge){
+        edgesSorted.add(edge);
+    }
 
+    public ArrayList<Edge> getEdgesSorted() {
+        return edgesSorted;
+    }
+    public void setToArrayList(Edge edge){
+        edgesSorted.add(edge);
+    }
+    public void clearSortedAL(){
+        edgesSorted.clear();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(obj == this) return true;
+        if(!(obj instanceof Node)) return false;
+        Node o = (Node) obj;
+        return o.name == this.name;
+    }
 }
 
 
